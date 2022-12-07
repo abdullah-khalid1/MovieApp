0eeafb5dbda16a5e48bc3bb95e28a17d6c8895c2
Abdullah Khalid <abdullahkhalid@Khalids-MacBook-Pro.local>
2022-12-07 17:19:26 +0100

    Project Structure Created

---
+++ android/app/src/main/kotlin/com/example/myblocpractice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myblocpractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:myblocpractice/src/app.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
